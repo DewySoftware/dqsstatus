6b0ad2d14d8821f149fdcf4dc7fd727c06c17d03
Dewy <dewy@dewy.dev>
2020-07-25 14:44:22 +0100

    bump 1.0.0

--- src/main/java/dev/dewy/dqs/status/Main.java
+++ src/main/java/dev/dewy/dqs/status/Main.java
@@ -21,7 +21,7 @@ import java.io.Reader;
 
 public class Main
 {
-    public static final String VERSION = "1.0.0-dev";
+    public static final String VERSION = "1.0.0";
 
     public static JDA discord;
 
