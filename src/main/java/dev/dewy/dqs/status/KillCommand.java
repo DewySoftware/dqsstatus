943ece6f6139437e77fcb53c37b2cd1477177156
Dewy <dewy@dewy.dev>
2020-07-25 14:22:38 +0100

    get started on the bot

---
+++ src/main/java/dev/dewy/dqs/status/KillCommand.java
@@ -0,0 +1,61 @@
+package dev.dewy.dqs.status;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.awt.*;
+
+public class KillCommand extends Command
+{
+    public KillCommand()
+    {
+        this.name = "kill";
+        this.guildOnly = false;
+    }
+
+    @Override
+    protected void execute(CommandEvent event)
+    {
+        try
+        {
+            if (!event.getAuthor().getId().equals(Main.CONFIG.status.ownerId))
+            {
+                event.reply(new EmbedBuilder()
+                        .setTitle("**DQS Status** - Insufficient Permissions")
+                        .setDescription("You do not have the required permissions to execute this command.")
+                        .setColor(new Color(15221016))
+                        .setAuthor("DQS Status " + Main.VERSION, null, "https://i.imgur.com/pcSOd3K.png")
+                        .build());
+
+                return;
+            }
+
+            if (event.getChannelType().isGuild())
+            {
+                event.reply(new EmbedBuilder()
+                        .setTitle("**DQS Status** - Must Be Executed In DMs")
+                        .setDescription("This command can not be ran in a guild channel.")
+                        .setColor(new Color(15221016))
+                        .setAuthor("DQS Status " + Main.VERSION, null, "https://i.imgur.com/pcSOd3K.png")
+                        .build());
+
+                return;
+            }
+
+            event.reply(new EmbedBuilder()
+                    .setTitle("**DQS Status** - Kill")
+                    .setDescription("Shutting down...")
+                    .setColor(new Color(10144497))
+                    .setAuthor("DQS Status " + Main.VERSION, null, "https://i.imgur.com/pcSOd3K.png")
+                    .build());
+
+            Thread.sleep(2000L);
+
+            Runtime.getRuntime().exit(0);
+        } catch (Exception e)
+        {
+            Main.LOGGER.error(e);
+        }
+    }
+}
--- src/main/java/dev/dewy/dqs/status/Main.java
+++ src/main/java/dev/dewy/dqs/status/Main.java
@@ -2,11 +2,17 @@ package dev.dewy.dqs.status;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import net.daporkchop.lib.binary.oio.reader.UTF8FileReader;
 import net.daporkchop.lib.common.misc.file.PFiles;
 import net.daporkchop.lib.logging.Logging;
 import net.daporkchop.lib.logging.impl.DefaultLogger;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 
+import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
@@ -15,16 +21,55 @@ public class Main
 {
     public static final String VERSION = "1.0.0-dev";
 
+    public static JDA discord;
+
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     public static final File CONIFG_FILE = new File(System.getProperty("user.dir") + "/config.json");
+
     public static final DefaultLogger LOGGER = Logging.logger;
+
     public static Config CONFIG;
 
+    @SuppressWarnings("all")
     public static void main(String[] args)
     {
         LOGGER.info("Starting DQS Status v%s...", VERSION);
 
         loadConfig();
+
+        if (CONFIG.status.token.equals("default") || CONFIG.status.ownerId.equals("default"))
+        {
+            LOGGER.error("Please enter your configuration values properly.");
+
+            Runtime.getRuntime().exit(-1);
+        }
+
+        CommandClientBuilder commandClient = new CommandClientBuilder();
+
+        commandClient.setPrefix("&");
+        commandClient.setActivity(Activity.watching("DQS"));
+        commandClient.setOwnerId(CONFIG.status.ownerId);
+
+        commandClient.addCommands(
+                new KillCommand()
+        );
+
+        commandClient.setHelpWord("JBjEFIWHB213&£YRf8ucjfhu8Y£YRH8usah");
+
+        try
+        {
+            LOGGER.info("Starting Discord bot...");
+
+            discord = new JDABuilder(AccountType.BOT)
+                    .setToken(CONFIG.status.token)
+                    .addEventListeners(commandClient.build())
+                    .build();
+        } catch (LoginException e)
+        {
+            LOGGER.error(e);
+
+            Runtime.getRuntime().exit(-1);
+        }
     }
 
     public static void loadConfig()
